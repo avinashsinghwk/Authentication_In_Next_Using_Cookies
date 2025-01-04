3a5a2c4409c7640c938e32f30afb8e007798a9a1
Avinash Kumar Singh <abhinashsinghwk2@gmail.com>
2025-01-04 14:22:29 +0530

    add logic to verifyemail backend

---
+++ src/app/api/(auth)/verifyemail/route.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/utils/db";
+
+export async function POST(req: NextRequest) {
+
+    const { otp } = await req.json()
+    const id = req.nextUrl.searchParams.get('id')
+
+    if (!otp || !id) {
+        return NextResponse.json({
+            success: false,
+            message: 'Send otp and id'
+        }, { status: 403 })
+    }
+
+    try {
+        const isUser = await prisma.user.findFirst({ where: { id } })
+
+        if (!isUser) {
+            return NextResponse.json({
+                success: false,
+                message: 'This user is not present'
+            }, { status: 403 })
+        }
+
+        if (!isUser.verifyOtp || !isUser.verifyOtpExpiresAt) {
+            return NextResponse.json({
+                success: false,
+                message: 'Send otp again'
+            }, { status: 401 })
+        }
+
+        if (isUser.verifyOtp == otp && Number(isUser.verifyOtpExpiresAt) > Date.now()) {
+
+            const user = await prisma.user.update({
+                where: { id },
+                data: { isVerified: true, verifyOtp: null, verifyOtpExpiresAt: null }
+            })
+
+            return NextResponse.json({
+                success: true,
+                message: 'User verified',
+                id: user.id
+            }, { status: 200 })
+            
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: 'Send otp again'
+            }, { status: 401 })
+        }
+
+    } catch (e) {
+        console.log(`Some error happened in verifying email : ${e}`)
+        return NextResponse.json({
+            success: false,
+            message: 'Unable to verify'
+        }, { status: 500 })
+    }
+}
